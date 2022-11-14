ae8927efd09a30e7c0ada5e705339def0b952925
Rustam <rustam.zholdubayev@gmail.com>
2022-11-14 13:15:55 -0500

    2nd commit

--- sea_house/settings.py
+++ sea_house/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'houses.apps.HousesConfig',
-    'sorl.thumbnail'
+    'sorl.thumbnail',
+    'orders'
 ]
 
 MIDDLEWARE = [
@@ -56,7 +57,7 @@ ROOT_URLCONF = 'sea_house.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
