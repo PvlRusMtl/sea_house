94bdc3cd6b2610b7d7f2531f86e60d59c4d23e5d
Rustam <rustam.zholdubayev@gmail.com>
2022-11-15 13:41:06 -0500

    3rd commit

--- houses/models.py
+++ houses/models.py
@@ -5,11 +5,12 @@ class House(models.Model):
     price = models.IntegerField('price')
     description = models.TextField('description')
     image = models.ImageField('image', upload_to='houses\images', default='', blank=True)
+    active = models.BooleanField('active', default=True)
 
     class Meta:
         verbose_name = 'house'
         verbose_name_plural = 'houses'
-        ordering = ["name"]
+        ordering = ['-active', "name"]
 
     def __str__(self):
         return self.name.upper()
