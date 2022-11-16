f924f320eef3c9701b7e2ce0da1bfd958e7983e2
Rustam <rustam.zholdubayev@gmail.com>
2022-11-16 14:34:40 -0500

    4th commit

--- sea_house/settings.py
+++ sea_house/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -37,8 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'houses.apps.HousesConfig',
     'sorl.thumbnail',
+    'django.contrib.humanize',
+    'houses',
     'orders'
 ]
 
@@ -121,6 +123,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
 
 MEDIA_ROOT = BASE_DIR / 'media'
 MEDIA_URL = "/media/"
